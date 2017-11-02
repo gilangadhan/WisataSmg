ce3eb908fcc54b570c9844fd5a3d81fc4d175180
GILANG-PC\gilan <gilangramadhan96.gr@gmail.com>
2017-11-02 16:35:41 +0700

    mantabas

--- app/src/main/java/org/sandec/wisatasmg/networking/ApiServices.java
+++ app/src/main/java/org/sandec/wisatasmg/networking/ApiServices.java
@@ -2,8 +2,14 @@ package org.sandec.wisatasmg.networking;
 
 import org.sandec.wisatasmg.model.ListWisataModel;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 /**
  * Created by idn on 10/30/2017.
@@ -12,4 +18,16 @@ import retrofit2.http.GET;
 public interface ApiServices {
     @GET("read_wisata.php")
     Call<ListWisataModel> ambilDataWisata();
+
+    @Multipart
+    @POST("create_wisata.php")
+    Call<ResponseBody> CREATE_WISATA(@Part MultipartBody.Part file,
+                                     @Part("nama_wisata") RequestBody nama_wisata,
+                                     @Part("gambar_wisata") RequestBody gambar_wisata,
+                                     @Part("deksripsi_wisata") RequestBody deksripsi_wisata,
+                                     @Part("event_wisata") RequestBody event_wisata,
+                                     @Part("longitude_wisata") RequestBody longitude_wisata,
+                                     @Part("latitude_wisata") RequestBody latitude_wisata,
+                                     @Part("alamat_wisata") RequestBody alamat_wisata);
+
 }
